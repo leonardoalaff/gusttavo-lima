bcd76e42968aa667d1c1ac58d2b5f84c67957228
Leonardo Alaff <leonardoalaff@gmail.com>
2023-12-11 02:09:41 -0300

    1.5

---
+++ conf-jvs.js
@@ -0,0 +1,11 @@
+function desfoque() {
+    document.getElementById('video').className = 'video2-s1'
+    document.getElementById('palmas').className = 'palmas2'
+    document.getElementById('plataformas').className = 'plataformas2'
+}
+
+function foco() {
+    document.getElementById('video').className = 'video-s1'
+    document.getElementById('palmas').className = 'palmas'
+    document.getElementById('plataformas').className = 'plataformas'
+}
